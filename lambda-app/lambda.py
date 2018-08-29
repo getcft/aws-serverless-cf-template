d0988e6a938635a49e047977fafe1bc7fe782afb
Phil Chen <nethacker@gmail.com>
2018-08-29 16:02:25 -0700

    Added author for license

--- lambda-app/lambda.py
+++ lambda-app/lambda.py
@@ -1,5 +1,5 @@
-#Copyright (c) 2018 Phil Chen All rights reserved.
-#This work is licensed under the terms of the MIT license.  
+#Copyright (c) 2018 Author Phil Chen All rights reserved.
+#This work is licensed under the terms of the MIT license.
 
 # Lambda that returns JSON
 
