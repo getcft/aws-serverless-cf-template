f27b3dcc183cac9f4e3d39b592a29aac6b70437b
Phil Chen <nethacker@gmail.com>
2018-08-29 15:57:39 -0700

    Adding lambda app zip and non zip that produces example json

---
+++ lambda-app/lambda.py
@@ -0,0 +1,8 @@
+#Copyright (c) 2018 Phil Chen All rights reserved.
+#This work is licensed under the terms of the MIT license.  
+
+# Lambda that returns JSON
+
+def endpoint(event, context):
+    output = { "data": {"a": 1, "b": 2, "c": 3} }
+    return output
